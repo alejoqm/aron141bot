190c21ac5de9e10b0c622af887eba6e170413781
Diego LAA <odinnordico@users.noreply.github.com>
2018-10-03 17:11:38 -0500

    Update PokemonSearch.js
    error handler of pokedesk reequest

--- classes/PokemonSearch.js
+++ classes/PokemonSearch.js
@@ -1,6 +1,7 @@
 var scrapy = require('node-scrapy')
 var baseUrl = "https://www.pokemon.com/us/pokedex/";
 var selector = ".version-descriptions .version-y";
+var errorMsg = "Really!! ese pokemon no existe, te buscare y te mordere";
 class PokemonSearch {
    constructor() {
    }
@@ -8,13 +9,19 @@ class PokemonSearch {
 	search(pokemon, callback, message, res) {
 		console.log(pokemon)
 		var result = "";
-		scrapy.scrape('https://www.pokemon.com/us/pokedex/' + pokemon, selector, function(err, data) {
-			console.log(err + " " + data)
-			result = err !== null ? err : data;
-			result = Array.isArray(result) ? result[0] : result;
-			console.log("data " + result)
-			callback(message, result, res);
-		});	
+		try {
+			scrapy.scrape('https://www.pokemon.com/us/pokedex/' + pokemon, selector, function(err, data) {
+				console.log(err + " " + data)
+				result = err !== null ? err : data;
+				result = Array.isArray(result) ? result[0] : result;
+				console.log("data " + result)
+				callback(message, result, res);
+			});
+		}
+		catch(err) {
+			console.log(err)
+			result = errorMsg;
+		}	
 	}
 }
 module.exports = PokemonSearch;

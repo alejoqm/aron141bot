481d1d85d9d1bbc289a3ddbaf0f96f07c49ee73d
Diego Alfonso <diego.alfonso@wnco.com>
2018-10-12 16:06:36 -0500

    Added nick validation

--- classes/NickNameValidation.js
+++ classes/NickNameValidation.js
@@ -5,6 +5,8 @@ class NickNameValidation {
     saySomegthing(message) {
         if(message.from.username == 'DiegoLAA90')
             return "Chupelo";
+        else if (message.from.username.toLowerCase().includes('Alejoqm'.toLowerCase()))
+            return "@" + message.from.username + " deje de enseñarme pendejadas y  literalmente maricadas!";
         else
             return "";
     }

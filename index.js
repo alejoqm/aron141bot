ea7ffe979a0697f84a4d1a762ca9171b3dafe914
alejoqm <alejandroq@blanclabs.com>
2018-10-17 17:10:12 -0500

    [AQ] Addlog

--- index.js
+++ index.js
@@ -25,9 +25,12 @@ app.use(
 //This is the route the API will call
 app.post('/new-message', function(req, res) {
   const { message } = req.body
+  var subjectName = subjectName.getSubjectName(message.text);
+  var offensive = offensive.hasOffensiveContent(message.text);
+  console.log(subjectName + " " + offensive)
   if(message.text == undefined) {
       res.end();
-  } else if(subjectName.getSubjectName(message.text) == "aron" && offensive.hasOffensiveContent(message.text)) {
+  } else if(ignoreCase.equals("aron", subjectName) && offensive) {
     publish(message, "Are you talking with me?", res);
   } else {
       console.log("New request " + message.text)
@@ -40,6 +43,7 @@ app.post('/new-message', function(req, res) {
 // Finally, start our server
 app.listen((process.env.PORT || 3000), function() {
   console.log('Telegram app listening on port !' + (process.env.PORT || 3000))
+    var text = "aron gay"
 })
 
 function publish(message, responsemessage, res) {

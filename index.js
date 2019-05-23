ccc71c87b49a75cacee58d9fd43ddc9550b1b2a2
alejoqm <alejandroq@blanclabs.com>
2019-05-23 16:10:24 -0500

    Validate for spam

--- index.js
+++ index.js
@@ -36,7 +36,10 @@ app.post('/new-message', function (req, res) {
         console.log("Message too old or undefined.")
         res.end();
     } else {
-        if (message.photo !== undefined) {
+        if(message.forward_from !== undefined || message.forward_from_chat !== undefined) {
+            console.log('This message was forwarded');
+            publish(message, 'Seriusly ' + message.from.username + ', generando spam!!');
+        } else if (message.photo !== undefined) {
             console.log(message.photo);
             //publish(message, "Que linda foto!", res)
             res.end();
